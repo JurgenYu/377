aed7429d2d14fd71923ccdbb4e79e1dcaef82e52
jurgen <tigeryhw@gmail.com>
2020-03-05 06:53:17 -0500

    0305

--- p3-student/src/BoundedBuffer.cpp
+++ p3-student/src/BoundedBuffer.cpp
@@ -1,23 +1,56 @@
 #include "BoundedBuffer.h"
 
 BoundedBuffer::BoundedBuffer(int N) {
-  //TODO: constructor to initiliaze all the varibales declared in BoundedBuffer.h
+    // TODO: constructor to initiliaze all the varibales declared in
+    // BoundedBuffer.h
+    buffer_size = N;
+    buffer_cnt = 0;
+    buffer_first = 0;
+    buffer_last = 0;
+    int buffer_array[buffer_size];
+    buffer = buffer_array;
+    pthread_mutex_init(&buffer_lock, NULL);
+    pthread_cond_init(&buffer_not_full, NULL);
+    pthread_cond_init(&buffer_not_empty, NULL);
 }
 
 BoundedBuffer::~BoundedBuffer() {
-  //TODO: destructor to clean up anything necessary
+    // TODO: destructor to clean up anything necessary
+    pthread_mutex_destroy(&buffer_lock);
+    pthread_cond_destroy(&buffer_not_empty);
+    pthread_cond_destroy(&buffer_not_full);
 }
 
 void BoundedBuffer::append(int data) {
-  //TODO: append a data item to the circular buffer
+    // TODO: append a data item to the circular buffer
+    pthread_mutex_lock(&buffer_lock);
+    while (buffer_cnt==buffer_size)
+    {
+        pthread_cond_wait(&buffer_not_full, &buffer_lock);
+    }
+    buffer[buffer_last] = data;
+    buffer_last = (buffer_last + 1) % buffer_size;
+    ++buffer_cnt;
+    pthread_cond_signal(&buffer_not_empty);
+    pthread_mutex_unlock(&buffer_lock);
 }
 
 int BoundedBuffer::remove() {
-  //TODO: remove and return a data item from the circular buffer
-  return -1;
+    // TODO: remove and return a data item from the circular buffer
+    pthread_mutex_lock(&buffer_lock);
+    int data;
+    while (isEmpty())
+    {
+        pthread_cond_wait(&buffer_not_empty, NULL);
+    }
+    data = buffer[buffer_first];
+    buffer_first = (buffer_first + 1) % buffer_size;
+    pthread_cond_signal(&buffer_not_full);
+    pthread_mutex_unlock(&buffer_lock);
+    return data;
 }
 
 bool BoundedBuffer::isEmpty() {
-  //TODO: check is the buffer is empty
-  return true;
+    // TODO: check is the buffer is empty
+    return buffer_size == 0;
 }
--- p3-student/src/ProducerConsumer.cpp
+++ p3-student/src/ProducerConsumer.cpp
@@ -2,15 +2,74 @@
 
 // TODO: add BoundedBuffer, locks and any global variables here
 
+BoundedBuffer buffer;
+int p_cnt = 0;
+int c_cnt = 0;
+int ps;
+int cs;
+auto start = std::chrono::steady_clock::now();
+int item_size;
+pthread_mutex_t p_lock;
+pthread_mutex_t c_lock;
+
 void InitProducerConsumer(int p, int c, int psleep, int csleep, int items) {
-  //TODO: constructor to initialize variables declared
-  //also see instruction for implementation
+  // TODO: constructor to initialize variables declared
+  // also see instruction for implementation
+  BoundedBuffer buffer(10);
+  ps = psleep;
+  cs = csleep;
+  item_size = items;
+  pthread_mutex_init(&p_lock, NULL);
+  pthread_mutex_init(&c_lock, NULL);
+  pthread_t plist[p];
+  pthread_t clist[c];
+  for (int i = 0; i < p; i++) {
+    int out = pthread_create(&plist[i], NULL, producer, &i);
+    if (out) {
+      perror("Producer Initiation Error");
+    }
+  }
+  for (int j = 0; j < c; j++) {
+    int out = pthread_create(&clist[j], NULL, consumer, &j);
+    if (out) {
+      perror("Consumer Initiation Error");
+    }
+  }
+  for (int a = 0; a < p; a++) {
+    int out = pthread_join(plist[a], NULL);
+    if (out) {
+      perror("Producer Join Error");
+    }
+  }
+  for (int b = 0; b < c; b++) {
+    int out = pthread_join(clist[b], NULL);
+    if (out) {
+      perror("Consumer Join Error");
+    }
+  }
+  pthread_mutex_destroy(&p_lock);
+  pthread_mutex_destroy(&c_lock);
 }
 
-void* producer(void* threadID) {
-  //TODO: producer thread, see instruction for implementation
+void *producer(void *threadID) {
+  int produced = 0;
+  while (1) {
+    usleep(ps);
+    pthread_mutex_lock(&p_lock);
+    if (p_cnt == item_size)
+      ;
+    { break; }
+    produced+=1;
+    int data = rand() % 100;
+    buffer.append(data);
+    ++p_cnt;
+    cout << "Producer #" << threadID << ", ";
+    time = current time, producing data item #j,
+                         item value = foo pthread_mutex_unlock(&p_lock);
+  }
+  // TODO: producer thread, see instruction for implementation
 }
 
-void* consumer(void* threadID) {
-  //TODO: consumer thread, see instruction for implementation
+void *consumer(void *threadID) {
+  // TODO: consumer thread, see instruction for implementation
 }
